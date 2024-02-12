71bc99657361f41b3816e8f9e23d685cbc2b9202
Vlad Sunny <vladislavsunny@gmail.com>
2024-02-12 19:12:24 +0300

    requirements

---
+++ main.py
@@ -0,0 +1,33 @@
+#start: export FLASK_APP=app.py && python3 -m flask run --host=0.0.0.0 --cert=adhoc
+
+from flask import Flask, request
+import logging
+import json
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/", methods=["POST"])
+def main():
+    logging.info(request.json)
+
+    response = {
+        "version": request.json["version"],
+        "session": request.json["session"],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    req = request.json
+    if req["session"]["new"]:
+        response["response"]["text"] = "Привет! Как твои дела? Как отметил новый год?"
+    else:
+        if req["request"]["original_utterance"].lower() in ["хорошо", "отлично"]:
+            response["response"]["text"] = "Супер! Я за вас рада!"
+        elif req["request"]["original_utterance"].lower() in ["плохо", "скучно"]:
+            response["response"]["text"] = "Это печально... нужно было позвать меня!"
+
+    return json.dumps(response)
