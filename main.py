c8d1aa771a6ad9d2c05037b5a1e215b93ef48e51
Vlad Sunny <vladislavsunny@gmail.com>
2024-02-12 19:41:42 +0300

    Fixes

--- main.py
+++ main.py
@@ -12,23 +12,23 @@ def main():
     logging.info(request.json)
     print(request.json)
 
-    # response = {
-    #     "version": request.json["version"],
-    #     "session": request.json["session"],
-    #     "response": {
-    #         "end_session": False
-    #     }
-    # }
-    #
-    # req = request.json
-    # if req["session"]["new"]:
-    #     response["response"]["text"] = "Привет! Как твои дела? Как отметил новый год?"
-    # else:
-    #     if req["request"]["original_utterance"].lower() in ["хорошо", "отлично"]:
-    #         response["response"]["text"] = "Супер! Я за вас рада!"
-    #     elif req["request"]["original_utterance"].lower() in ["плохо", "скучно"]:
-    #         response["response"]["text"] = "Это печально... нужно было позвать меня!"
-    #
+    response = {
+        "version": request.json["version"],
+        "session": request.json["session"],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    req = request.json
+    if req["session"]["new"]:
+        response["response"]["text"] = "Привет! Как твои дела? Как отметил новый год?"
+    else:
+        if req["request"]["original_utterance"].lower() in ["хорошо", "отлично"]:
+            response["response"]["text"] = "Супер! Я за вас рада!"
+        elif req["request"]["original_utterance"].lower() in ["плохо", "скучно"]:
+            response["response"]["text"] = "Это печально... нужно было позвать меня!"
+
     return json.dumps("{}")
 
 
