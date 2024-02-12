1af599f4f68800b355300a31cc18d97f82bc0c79
Vlad Sunny <vladislavsunny@gmail.com>
2024-02-12 19:30:54 +0300

    requirements

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#start: export FLASK_APP=app.py && python3 -m flask run --host=0.0.0.0 --cert=adhoc
-
 from flask import Flask, request
 import logging
 import json
@@ -12,22 +10,27 @@ logging.basicConfig(level=logging.DEBUG)
 @app.route("/", methods=["POST"])
 def main():
     logging.info(request.json)
+    print(request.json)
 
-    response = {
-        "version": request.json["version"],
-        "session": request.json["session"],
-        "response": {
-            "end_session": False
-        }
-    }
+    # response = {
+    #     "version": request.json["version"],
+    #     "session": request.json["session"],
+    #     "response": {
+    #         "end_session": False
+    #     }
+    # }
+    #
+    # req = request.json
+    # if req["session"]["new"]:
+    #     response["response"]["text"] = "Привет! Как твои дела? Как отметил новый год?"
+    # else:
+    #     if req["request"]["original_utterance"].lower() in ["хорошо", "отлично"]:
+    #         response["response"]["text"] = "Супер! Я за вас рада!"
+    #     elif req["request"]["original_utterance"].lower() in ["плохо", "скучно"]:
+    #         response["response"]["text"] = "Это печально... нужно было позвать меня!"
+    #
+    return json.dumps("{}")
 
-    req = request.json
-    if req["session"]["new"]:
-        response["response"]["text"] = "Привет! Как твои дела? Как отметил новый год?"
-    else:
-        if req["request"]["original_utterance"].lower() in ["хорошо", "отлично"]:
-            response["response"]["text"] = "Супер! Я за вас рада!"
-        elif req["request"]["original_utterance"].lower() in ["плохо", "скучно"]:
-            response["response"]["text"] = "Это печально... нужно было позвать меня!"
 
-    return json.dumps(response)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
